d49495fc999346b37a0c98aa381f3933398292d9
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-26 05:54:57 +0200

    shooting mechanic

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float Range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, Range);
+        Debug.Log("I hit " + hit.transform.name);
+    }
+
+    
+
+}
