83af4a77aad242ce5757fe5f0b102bbccda9813c
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-27 18:49:41 +0200

    weapon switcher

--- Assets/Scripts/DeathHandler.cs
+++ Assets/Scripts/DeathHandler.cs
@@ -18,6 +18,7 @@ public class DeathHandler : MonoBehaviour
         GetComponent<StarterAssetsInputs>().cursorLocked = false;
         GetComponent<FirstPersonController>().RotationSpeed = 0f;
         Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
